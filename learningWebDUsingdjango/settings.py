3443055d92e39d593facc43e11b4344b586710ce
Mukul_kYadav <ymukul6180@gmail.com>
2022-10-07 14:08:31 +0530

    tasks app created

--- learningWebDUsingdjango/settings.py
+++ learningWebDUsingdjango/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'learnapp',
     'diwali',
+    'tasks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- learningWebDUsingdjango/urls.py
+++ learningWebDUsingdjango/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('learnapp/', include("learnapp.urls")),
     path('diwali/', include("diwali.urls")),
+    path('tasks/', include("tasks.urls")),
 ]
